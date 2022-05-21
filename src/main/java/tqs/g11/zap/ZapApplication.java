4a653f356f55b322e571693f19f5a9bb9da14441
inryatt <cffonseca@ua.pt>
2022-05-21 12:00:33 +0100

    Initialized Spring Project

---
+++ src/main/java/tqs/g11/zap/ZapApplication.java
@@ -0,0 +1,13 @@
+package tqs.g11.zap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapApplication.class, args);
+	}
+
+}
